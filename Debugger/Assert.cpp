9548daa3837210f3994f8b0f55ea21afd0b2a3d8
shishir993 <shishir89@gmail.com>
2014-03-18 21:59:34 -0700

    Added hard breakpoint

--- Debugger/Assert.cpp
+++ Debugger/Assert.cpp
@@ -8,5 +8,14 @@ void vAssert(const char* pszFile, UINT uLine)
 	fprintf(stderr, "Assertion failed in %s at Line %u\n", pszFile, uLine);
 	fflush(stderr);
 
+    // Adding a hard breakpoint because:
+    // In GUI programs, there is usually no console window open to display the assertion failure.
+    // Even when a GUI program explicitly opens a console window, exiting the program causes
+    // the console also to be closed immediately. So we cannot see where the assertion failed.
+
+#ifdef _DEBUG       // just in case this code is called without using ASSERT macro
+    __asm int 3        
+#endif
+
 	exit(CE_ASSERT_FAILED);
 }
